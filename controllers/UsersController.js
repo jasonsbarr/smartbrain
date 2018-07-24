b8452e99e538a8240f9ff1b4671096b3a37bc3c1
Jason Barr <jason@jasonsbarr.com>
2018-07-24 18:29:17 -0500

    Refactor User CRUD & login methods to controller

---
+++ controllers/UsersController.js
@@ -0,0 +1,84 @@
+const index = (_, res, db) => {
+  db.select('*').from('users').orderBy('id', 'asc')
+    .then(users => {
+      if (users.length) {
+        res.json(users);
+      } else {
+        res.status(404).json('No users found.');
+      }
+    })
+    .catch(err => res.status(400).json('There was a problem.'));
+};
+
+const get = (req, res, db) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+  .then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(404).json('User not found.');
+    }
+  })
+  .catch(err => res.status(400).json('Something went wrong.'));
+};
+
+const create = (req, res, db, bcrypt) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+
+  db.transaction(trx => {
+    trx.insert({
+      hash,
+      email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx.insert({
+        email: loginEmail[0],
+        name,
+        joined: new Date()
+        })
+        .into('users')
+        .returning('*')
+        .then(user => res.json(user[0]))
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json("Unable to register."));
+};
+
+const login = (req, res, db, bcrypt) => {
+  db.select('email', 'hash').from('login').where('email', req.body.email)
+  .then(data => {
+    if (bcrypt.compareSync(req.body.password, data[0].hash)) {
+      return db.select('*').from('users').where('email', req.body.email)
+      .then(user => res.json(user[0]))
+      .catch(err => res.status(400).json('Error fetching user.'));
+    }
+    res.status(401).json('Email/password combination is invalid.');
+  })
+};
+
+const updateEntries = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where({id}).increment('entries', 1).returning('entries')
+    .then(entries => {
+      if (entries.length) {
+        res.json(entries[0]);
+      } else {
+        res.status(404).json('User not found.');
+      }
+    })
+    .catch(err => res.status(400).json('Something went wrong.'));
+};
+
+module.exports = {
+  index,
+  get,
+  create,
+  login,
+  updateEntries
+};
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const Express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = Express();
 
 const db = knex({
@@ -15,85 +16,20 @@ const db = knex({
   } 
 });
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  db.select('*').from('users').orderBy('id', 'asc')
-    .then(users => {
-      if (users.length) {
-        res.json(users);
-      } else {
-        res.status(404).json('No users found.');
-      }
-    })
-    .catch(err => res.status(400).json('There was a problem.'));
-});
+const UsersController = require('./controllers/UsersController');
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-  .then(user => {
-    if (user.length) {
-      res.json(user[0]);
-    } else {
-      res.status(404).json('User not found.');
-    }
-  })
-  .catch(err => res.status(400).json('Something went wrong.'));
-});
+app.get('/', (req, res) => UsersController.updateEntries(req, res, db));
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login').where('email', req.body.email)
-  .then(data => {
-    if (bcrypt.compareSync(req.body.password, data[0].hash)) {
-      return db.select('*').from('users').where('email', req.body.email)
-      .then(user => res.json(user[0]))
-      .catch(err => res.status(400).json('Error fetching user.'));
-    }
-    res.status(401).json('Email/password combination is invalid.');
-  })
-});
+app.get('/profile/:id', (req, res) => UsersController.updateEntries(req, res, db));
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
+app.post('/signin', (req, res) => UsersController.login(req, res, db, bcrypt));
 
-  db.transaction(trx => {
-    trx.insert({
-      hash,
-      email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx.insert({
-        email: loginEmail[0],
-        name,
-        joined: new Date()
-        })
-        .into('users')
-        .returning('*')
-        .then(user => res.json(user[0]))
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json("Unable to register."));
-});
+app.post('/register', (req, res) => UsersController.create(req, res, db, bcrypt));
 
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where({id}).increment('entries', 1).returning('entries')
-    .then(entries => {
-      if (entries.length) {
-        res.json(entries[0]);
-      } else {
-        res.status(404).json('User not found.');
-      }
-    })
-    .catch(err => res.status(400).json('Something went wrong.'));
-});
+app.put('/image', (req, res) => UsersController.updateEntries(req, res, db));
 
 app.listen(3000, () => {
   console.log('App is running on port 3000');
