cfbb2ac0fb41a613ea14242d7b37b9cc0221489a
Jason Barr <jason@jasonsbarr.com>
2018-07-24 23:05:31 -0500

    Add example config files

---
+++ api/env.example.js
@@ -0,0 +1,13 @@
+const env = {
+  port: 'port (number)',
+  db: {
+    client: 'client',
+    host: 'host',
+    user: 'user',
+    password: 'password',
+    database: 'database'
+  },
+  clarifai: 'API key'
+};
+
+module.exports = env;
---
+++ ui/src/config.example.js
@@ -0,0 +1,5 @@
+const config = {
+  apiUrl: 'server base URL'
+};
+
+export default config;
